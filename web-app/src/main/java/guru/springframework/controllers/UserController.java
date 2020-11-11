f01bff32e9d3ae5e1396e98e799782e786d782b2
Wolf Developer <lupo.andrea.93@gmail.com>
2020-11-11 08:48:57 +0100

    Jaxb per creare classi da xml

---
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -0,0 +1,12 @@
+package guru.springframework.controllers;
+
+
+import guru.springframework.entities.User;
+
+public class UserController {
+
+    User saveUser() {
+        // fake impl
+        return new User();
+    }
+}
